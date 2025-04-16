4145ccd0498ad5df3613d046968407bfeb49e2ac
biswadipseth1980 <102617283+biswadipseth1980@users.noreply.github.com>
2025-04-16 15:42:00 -0400

    Add files via upload

---
+++ SQL/Customer_Landing.sql
@@ -0,0 +1,27 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer')
+COMMENT 'biswa created customer_landing'
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://biswa-aws-spark-datalake/customer/landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1744751587')
