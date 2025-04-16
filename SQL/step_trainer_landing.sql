34063cf18c90aeb68e5a01a9573ac5d146e46a76
biswadipseth1980 <102617283+biswadipseth1980@users.noreply.github.com>
2025-04-16 15:46:13 -0400

    Add files via upload

---
+++ SQL/step_trainer_landing.sql
@@ -0,0 +1,21 @@
+CREATE EXTERNAL TABLE `step_trainer_landing`(
+  `sensorreadingtime` bigint COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `distancefromobject` int COMMENT 'from deserializer')
+COMMENT 'biswa created table named "step_trainer_landing" for aws spark work'
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'case.insensitive'='TRUE', 
+  'dots.in.keys'='FALSE', 
+  'ignore.malformed.json'='FALSE', 
+  'mapping'='TRUE') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://biswa-aws-spark-datalake/step_trainer/landing'
+TBLPROPERTIES (
+  'classification'='json', 
+  'transient_lastDdlTime'='1744752189')
